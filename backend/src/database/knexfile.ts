0bd64bb32efc0dc38ec108a3e4f01fd66650b099
alexandresilvestri <airtontunni@gmail.com>
2025-12-12 09:29:17 -0800

    knex test env config to ci/cd

--- backend/src/database/knexfile.ts
+++ backend/src/database/knexfile.ts
@@ -28,6 +28,28 @@ const config: { [key: string]: Knex.Config } = {
       directory: './seeds',
     },
   },
+
+  test: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'pms-test',
+      port: Number(process.env.DB_PORT) || 5432,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './migrations',
+      extension: 'ts',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
 }
 
 export default config
