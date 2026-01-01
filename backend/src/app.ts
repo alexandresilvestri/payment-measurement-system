e7f34666dd728d4f7ab88c5352fa1fccb18b7373
AS-Gaspar <airtontunni@gmail.com>
2026-01-01 17:46:37 -0300

    git reset hard HEAD etc...

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,5 @@
-import dotenv from 'dotenv'
-import path from 'path'
-import { fileURLToPath } from 'url'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 import express from 'express'
 import cors from 'cors'
-// import cookieParser from 'cookie-parser'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import routes from './routes'
@@ -15,9 +8,7 @@ import { errorHandler, notFoundHandler } from './errors/middleware/errorHandler'
 const app = express()
 
 app.use(helmet())
-app.use(
-  cors()
-)
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
