2609551e7df725af62612edd4f066c030c907949
gaspar <airtontunni@gmail.com>
2026-01-05 19:55:47 -0300

    fix: logic error on get all user-types endpoint

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -6,16 +6,15 @@ import { createUserSchema } from '../validation/schemas/users.js'
 import { createUserHandler } from '../controllers/users.js'
 import usersRoutes from './users.js'
 import userTypesRoutes from './userTypes.js'
+import { getAllUserTypesHandler } from '../controllers/userTypes.js'
 import worksRoutes from './works.js'
 import suppliersRoutes from './suppliers.js'
-import { getUserTypeSchema } from '../validation/schemas/userTypes.js'
-import { getUserTypeHandler } from '../controllers/userTypes.js'
 
 const router = express.Router()
 
 router.use('/auth', authRoutes)
 router.post('/users', validate(createUserSchema), createUserHandler)
-router.get('/user-types', validate(getUserTypeSchema), getUserTypeHandler)
+router.get('/user-types', getAllUserTypesHandler)
 
 router.use(authenticate)
 router.use(usersRoutes)
